5603223805f9f1fd9b7162003bfb485ee6b7d454
Pavel <pavelzarov@yahoo.com>
2023-02-20 12:46:27 +0200

    learning hibernate

---
+++ src/main/java/pav/zar/hibernateapp/App.java
@@ -0,0 +1,33 @@
+package pav.zar.hibernateapp;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import pav.zar.hibernateapp.model.Person;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Configuration configuration = new Configuration().addAnnotatedClass(Person.class);
+
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
+
+        try {
+            session.beginTransaction();
+
+            Person person = session.get(Person.class,1);
+            System.out.println(person.getName());
+            System.out.println(person.getAge());
+
+            session.getTransaction().commit();
+        } finally {
+            sessionFactory.close();
+        }
+    }
+}
