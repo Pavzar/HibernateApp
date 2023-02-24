ac8cff1f693c91145eb0febf2fff6b5bc8daaf0e
Pavel <pavelzarov@yahoo.com>
2023-02-24 20:44:41 +0200

    cascading using save hibernate method and Cascade(CascadeType.SAVE_UPDATE) annotation

--- src/main/java/pav/zar/hibernateapp/App.java
+++ src/main/java/pav/zar/hibernateapp/App.java
@@ -27,13 +27,13 @@ public class App {
 
             Person person = new Person("Test cascading", 30);
 
-            Item item = new Item("Test cascading item", person);
-            person.setItems(new ArrayList<>(Collections.singletonList(item)));
+            person.addItem(new Item("item1"));
+            person.addItem(new Item("item2"));
+            person.addItem(new Item("item3"));
 
-            session.persist(person);
+            session.save(person);
 
             session.getTransaction().commit();
-
 
         } finally {
             sessionFactory.close();
